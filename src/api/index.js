e4e88ce57395e12153482bd8150c14142cdc9607
erickariasec <erickarias1999@hotmail.com>
2022-07-19 12:58:12 -0500

    Practicas-Erick-A, adding src, api, index.js: Generating API call to get all products

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,21 @@
 import fetch from "node-fetch";
+const API_products = "https://fakestoreapi.com/products";
+
+const fetchProducts = async (urlAPI) => {
+    const response = await fetch(urlAPI);
+    return response.json();
+}
+
+async function asyncGetProducts() {
+    try {
+
+        // ! GET ALL PRODUCTS
+        const products = await fetchProducts(`${API_products}`);
+        console.log(products);
+        
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+asyncGetProducts();
