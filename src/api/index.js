ad4c4b456733adc30e66c88542fbfaf9b472ac7b
erickariasec <erickarias1999@hotmail.com>
2022-07-19 13:00:12 -0500

    Practicas-Erick-A, adding src, api, index.js: Generating API call to get a single product

--- src/api/index.js
+++ src/api/index.js
@@ -12,7 +12,11 @@ async function asyncGetProducts() {
         // ! GET ALL PRODUCTS
         const products = await fetchProducts(`${API_products}`);
         console.log(products);
-        
+
+        // ! GET A SINGLE PRODUCT
+        const product1 = await fetchProducts(`${API_products}/1`);
+        console.log(product1);
+    
     } catch (err) {
         console.error(err);
     }
